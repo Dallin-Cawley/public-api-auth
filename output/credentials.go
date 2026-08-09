dd75675d7316a7a9e55712be43d5ed3d7972654c
timmy2654 <lettergrade39@gmail.com>
2026-08-09 11:29:20 -0600

    adding comprehensive output fields for dcr

--- output/credentials.go
+++ output/credentials.go
@@ -1,15 +1,63 @@
 package output
 
+import (
+	"github.com/Dallin-Cawley/public-api-auth/auth"
+	"github.com/Dallin-Cawley/public-api-auth/grant"
+	"github.com/Dallin-Cawley/public-api-auth/response"
+)
+
 // CreateCredentialsOutputBody provides the structure of the output body in the POST /credentials request
 type CreateCredentialsOutputBody struct {
-	ClientID     string `json:"client_id" doc:"The generated client ID"`
-	ClientSecret string `json:"client_secret" doc:"The generated client secret"`
+	ClientID                string         `json:"client_id" doc:"The generated client ID"`
+	ClientSecret            string         `json:"client_secret" doc:"The generated client secret"`
+	RedirectURIs            []string       `json:"redirect_uris,omitempty" schema:"redirect_uris" doc:"Array of redirection URIs"`
+	TokenEndpointAuthMethod auth.Method    `json:"token_endpoint_auth_method" schema:"token_endpoint_auth_method" doc:"Requested authentication method for the token endpoint"`
+	GrantTypes              grant.Types    `json:"grant_types" schema:"grant_types" enum:"client_credentials,authorization_code" doc:"The types of grant the credentials will be used for"`
+	ResponseTypes           response.Types `json:"response_types" schema:"response_types" enum:"code,token" doc:"The types of response the credentials will be used for"`
+	ClientName              string         `json:"client_name" schema:"client_name" doc:"Human-readable name of the client"`
+	ClientURI               string         `json:"client_uri" schema:"client_uri" doc:"URL of a web page providing information about the client"`
+	LogoURI                 string         `json:"logo_uri" schema:"logo_uri" doc:"URL that references a logo for the client application"`
+	Scopes                  []string       `json:"scopes" schema:"scopes" doc:"list of scope values"`
+	Contacts                []string       `json:"contacts" schema:"contacts" doc:"Array of strings, each containing an email address"`
+	TosURI                  string         `json:"tos_uri" schema:"tos_uri" doc:"URL that references a copy of the client's terms of service"`
+	PolicyURI               string         `json:"policy_uri" schema:"policy_uri" doc:"URL that references a copy of the client's privacy policy"`
+	JwksURI                 string         `json:"jwks_uri" schema:"jwks_uri" doc:"URL for the client's JSON Web Key Set [JWK] document"`
+	Jwks                    map[string]any `json:"jwks" schema:"jwks" doc:"Client's JSON Web Key Set [JWK] document, passed by value"`
+	SoftwareID              string         `json:"software_id" schema:"software_id" doc:"A unique identifier string assigned by the client developer"`
+	SoftwareVersion         string         `json:"software_version" schema:"software_version" doc:"A version identifier string for the client software"`
 }
 
 // NewCreateCredentialsOutputBody creates the output body for the POST /credentials request
-func NewCreateCredentialsOutputBody(clientID, clientSecret string) *CreateCredentialsOutputBody {
+func NewCreateCredentialsOutputBody(
+	clientID, clientSecret string,
+	redirectURIs []string,
+	tokenEndpointAuthMethod auth.Method,
+	grantTypes grant.Types,
+	responseTypes response.Types,
+	clientName, clientURI, logoURI string,
+	scopes []string,
+	contacts []string,
+	tosURI, policyURI, jwksURI string,
+	jwks map[string]any,
+	softwareID string, softwareVersion string,
+) *CreateCredentialsOutputBody {
 	return &CreateCredentialsOutputBody{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
+		ClientID:                clientID,
+		ClientSecret:            clientSecret,
+		RedirectURIs:            redirectURIs,
+		TokenEndpointAuthMethod: tokenEndpointAuthMethod,
+		GrantTypes:              grantTypes,
+		ResponseTypes:           responseTypes,
+		ClientName:              clientName,
+		ClientURI:               clientURI,
+		LogoURI:                 logoURI,
+		Scopes:                  scopes,
+		Contacts:                contacts,
+		TosURI:                  tosURI,
+		PolicyURI:               policyURI,
+		JwksURI:                 jwksURI,
+		Jwks:                    jwks,
+		SoftwareID:              softwareID,
+		SoftwareVersion:         softwareVersion,
 	}
 }
--- output/credentials_test.go
+++ output/credentials_test.go
@@ -3,6 +3,9 @@ package output
 import (
 	"testing"
 
+	"github.com/Dallin-Cawley/public-api-auth/auth"
+	"github.com/Dallin-Cawley/public-api-auth/grant"
+	"github.com/Dallin-Cawley/public-api-auth/response"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -13,11 +16,59 @@ type CredentialsTestSuite struct {
 func (testSuite *CredentialsTestSuite) TestNewCreateCredentialsOutputBody_Success() {
 	expectedClientID := "some client id"
 	expectedClientSecret := "some client secret"
+	expectedRedirectURIs := []string{"http://localhost:8080/callback"}
+	expectedTokenEndpointAuthMethod := auth.ClientSecretBasic
+	expectedGrantTypes := grant.Types{grant.ClientCredentials}
+	expectedResponseTypes := response.Types{response.Code}
+	expectedClientName := "some client name"
+	expectedClientURI := "http://localhost:8080"
+	expectedLogoURI := "http://localhost:8080/logo.png"
+	expectedScopes := []string{"read", "write"}
+	expectedContacts := []string{"test@example.com"}
+	expectedTosURI := "http://localhost:8080/tos"
+	expectedPolicyURI := "http://localhost:8080/policy"
+	expectedJwksURI := "http://localhost:8080/jwks"
+	expectedJwks := map[string]any{"key": "value"}
+	expectedSoftwareID := "some software id"
+	expectedSoftwareVersion := "some software version"
 
-	outputBody := NewCreateCredentialsOutputBody(expectedClientID, expectedClientSecret)
+	outputBody := NewCreateCredentialsOutputBody(
+		expectedClientID,
+		expectedClientSecret,
+		expectedRedirectURIs,
+		expectedTokenEndpointAuthMethod,
+		expectedGrantTypes,
+		expectedResponseTypes,
+		expectedClientName,
+		expectedClientURI,
+		expectedLogoURI,
+		expectedScopes,
+		expectedContacts,
+		expectedTosURI,
+		expectedPolicyURI,
+		expectedJwksURI,
+		expectedJwks,
+		expectedSoftwareID,
+		expectedSoftwareVersion,
+	)
 
 	testSuite.Equal(expectedClientID, outputBody.ClientID)
 	testSuite.Equal(expectedClientSecret, outputBody.ClientSecret)
+	testSuite.Equal(expectedRedirectURIs, outputBody.RedirectURIs)
+	testSuite.Equal(expectedTokenEndpointAuthMethod, outputBody.TokenEndpointAuthMethod)
+	testSuite.Equal(expectedGrantTypes, outputBody.GrantTypes)
+	testSuite.Equal(expectedResponseTypes, outputBody.ResponseTypes)
+	testSuite.Equal(expectedClientName, outputBody.ClientName)
+	testSuite.Equal(expectedClientURI, outputBody.ClientURI)
+	testSuite.Equal(expectedLogoURI, outputBody.LogoURI)
+	testSuite.Equal(expectedScopes, outputBody.Scopes)
+	testSuite.Equal(expectedContacts, outputBody.Contacts)
+	testSuite.Equal(expectedTosURI, outputBody.TosURI)
+	testSuite.Equal(expectedPolicyURI, outputBody.PolicyURI)
+	testSuite.Equal(expectedJwksURI, outputBody.JwksURI)
+	testSuite.Equal(expectedJwks, outputBody.Jwks)
+	testSuite.Equal(expectedSoftwareID, outputBody.SoftwareID)
+	testSuite.Equal(expectedSoftwareVersion, outputBody.SoftwareVersion)
 }
 
 func Test_RunCredentialsTestSuite(t *testing.T) {
